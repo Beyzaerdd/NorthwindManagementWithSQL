61fa55d5dd61b0f17cb8bce3841c97aa566011bd
Beyzaerdd <erdbeyza14@gmail.com>
2024-10-23 18:29:41 +0300

    Add files via upload

---
+++ NorthwindManagement/DataAccessLayer/Database.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindManagement.DataAccessLayer
+{
+    internal static class Database
+    {
+        public static SqlConnection Connection { get; set; }=CreateConnection();
+        private static SqlConnection CreateConnection()
+        {
+     
+            string connectionString = "Server=DESKTOP-48fdf8APMT\\SQLEXPRESS56502;";
+            connectionString += "Database=Northwind;";
+            connectionString += "User=bbbb;Password=123456487;";
+            connectionString += "TrustServerCertificate=true;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+        public static void ConnectDb()
+        {
+            if (Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+        }
+        public static void DisconnectDb()
+        {
+            if(Connection.State==ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+        }
+    }
+}
